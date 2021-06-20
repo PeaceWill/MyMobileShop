57697665423fa8d3e991cdb85d77cc70333e4ad7
PeaceWill <19521574@gm.uit.edu.vn>
2021-06-20 08:17:09 +0700

    update

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shop");
+define('DB_ADMIN_TABLE', 'admin');
+define('DB_USER_TABLE', 'user');
+define('DB_PRODUCT_TABLE', 'product');
+define('DB_PRODUCT_HOT_TABLE', 'product_hot');
+define('DB_ORDER_TABLE', 'bill');
+define('DB_ORDER_INFO_TABLE', 'bill_info');
+define('DB_CART_TABLE', 'cart');
+define('DB_FEEDBACK_TABLE', 'feedback');
+define('DB_CONTENT_INTRO', 'content_intro');
+define('DB_MEMBER_TABLE', 'member');
+define('DB_SOCIAL_TABLE', 'social_network');
+?>
---
+++ view/admin/useradd.php
@@ -0,0 +1,5 @@
+<?php 
+include 'layout/header.php';
+include 'layout/sidebar.php';
+include 'page/useradd.php'
+?>
